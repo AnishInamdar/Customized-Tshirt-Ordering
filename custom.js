254239d9f6ba5dd43841c2e05bbf34a9a2feb1d6
AnishInamdar <78248178+AnishInamdar@users.noreply.github.com>
2021-10-03 21:31:56 +0530

    Add files via upload

---
+++ custom.js
@@ -0,0 +1,15 @@
+function formValidation(){
+
+    //Input taken from user
+    const values = [
+        document.forms["myForm"]["design"].value,
+        document.forms["myForm"]["color"].value,
+        document.forms["myForm"]["printing"].value,
+        document.forms["myForm"]["size"].value,
+        document.forms["myForm"]["name"].value,
+        document.forms["myForm"]["number"].value,
+        document.forms["myForm"]["address"].value,
+        document.forms["myForm"]["pay"].value,
+    ];
+
+}
